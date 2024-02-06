e04932331dfcdf8afdb1d195460ab25eb9620892
MarioRadu <magda_marior@yahoo.com>
2024-02-06 14:50:10 +0200

    fixed command name issue

--- src/Command/DemoCommand.php
+++ src/Command/DemoCommand.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Dot\Cli\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'demo:command',
+    description: 'Demo command description.',
+)]
 class DemoCommand extends Command
 {
     /** @var string $defaultName */
